cf816f3afa5991c3a5a9a0f108c043f22953d3ec
Ketrel <ketrel@vauge.app>
2021-11-29 18:19:14 -0500

    Added Ace support and the option to reverse/swap the functionality

---
+++ BetterItemPreview/README.md
@@ -0,0 +1,6 @@
+This is a simple addon that modifies the ctrl+click to preview an item.
+
+If you Ctrl + Click an item, it previews as normal  
+If you Ctrl + Shift + Click an item, it previews the real item, not the transmogged appearance
+
+(MogIt, due to how it's coded, currently forces previewing the actual item.  This addon should take precedence over that behavior.)
--- BetterItemPreview/core.lua
+++ BetterItemPreview/core.lua
@@ -1,24 +1,63 @@
-function DressUpItemLocationReal(itemLocation)
+BIP = LibStub("AceAddon-3.0"):NewAddon("Better Item Preview")
+
+function BIP:OnInitialize()
+    local defaults = {
+        profile = {
+            reverse = false,
+        },
+    }
+
+    self.db = LibStub("AceDB-3.0"):New("BetterItemPreview", defaults, true)
+
+    local options = {
+        type = "group",
+        args = {
+            reverse = {
+                name = "Reverse Functionality",
+                desc = "Reverses the Ctrl+Click and Ctrl+Shift+Click Behavior",
+                type = "toggle",
+                descStyle = "inline",
+                width = "full",
+                get = function(info) return self.db.profile.reverse or false; end,
+                set = function(info,val) self.db.profile.reverse = val; end,
+            },
+        },
+    }
+
+    local profileOptions = LibStub("AceDBOptions-3.0"):GetOptionsTable(self.db)
+
+    LibStub("AceConfig-3.0"):RegisterOptionsTable("Better Item Preview", {type="group",args={}})
+    LibStub("AceConfig-3.0"):RegisterOptionsTable("Better Item Preview Options", options)
+    LibStub("AceConfig-3.0"):RegisterOptionsTable("Better Item Preview Profiles", profileOptions)
+
+    LibStub("AceConfigDialog-3.0"):AddToBlizOptions("Better Item Preview", "Better Item Preview", nil)
+    LibStub("AceConfigDialog-3.0"):AddToBlizOptions("Better Item Preview Options", "Options", "Better Item Preview")
+    LibStub("AceConfigDialog-3.0"):AddToBlizOptions("Better Item Preview Profiles", "Profiles", "Better Item Preview")
+
+
+    local originalHandleModifiedItemClick = HandleModifiedItemClick
+    HandleModifiedItemClick = function(link, itemLocation)
+        if IsModifiedClick("DRESSUP") and C_Item.IsDressableItemByID(link) then
+            if (IsShiftKeyDown() and not self.db.profile.reverse) or (not IsShiftKeyDown() and self.db.profile.reverse) and itemLocation then
+                return BIP.DressUpItemLocationReal(itemLocation) or DressUpItemLink(link) or DressUpBattlePet(link) or DressUpMount(link)
+            else
+                return DressUpItemLocation(itemLocation) or DressUpItemLink(link) or DressUpBattlePet(link) or DressUpMount(link)
+            end
+        else
+            originalHandleModifiedItemClick(link,itemLocation)
+        end
+    end
+
+end
+
+function BIP:DressUpItemLocationReal(itemLocation)
     if( itemLocation and itemLocation:IsValid() ) then
         local itemTransmogInfo = C_Item.GetCurrentItemTransmogInfo(itemLocation);
-		itemTransmogInfo:Clear()
+        itemTransmogInfo:Clear()
         -- non-equippable items won't have an appearanceID
         if itemTransmogInfo.appearanceID ~= Constants.Transmog.NoTransmogID then
             return DressUpItemTransmogInfo(itemTransmogInfo);
         end
     end
     return false;
-end
-
-local originalHandleModifiedItemClick = HandleModifiedItemClick
-HandleModifiedItemClick = function(link, itemLocation)
-    if IsModifiedClick("DRESSUP") and C_Item.IsDressableItemByID(link) then
-        if IsShiftKeyDown() and itemLocation then
-			return DressUpItemLocationReal(itemLocation) or DressUpItemLink(link) or DressUpBattlePet(link) or DressUpMount(link)
-		else
-			return DressUpItemLocation(itemLocation) or DressUpItemLink(link) or DressUpBattlePet(link) or DressUpMount(link)
-        end
-    else
-		originalHandleModifiedItemClick(link,itemLocation)
-	end
 end
